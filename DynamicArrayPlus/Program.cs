24b8add9f5accfea37c20176c573b86e9bafc5bc
DreaMeR-17 <pronka_1993@mail.ru>
2024-08-11 19:42:00 +0600

    Добавьте файлы проекта.

---
+++ DynamicArrayPlus/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayPlus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> userNumbers = new List<int>();
+
+            string commandExit = "exit";
+            string commandSum = "sum";
+            string userInput;
+
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.WriteLine("\nВведите ваши числа.");
+                Console.WriteLine($"Для суммирования чисел введите {commandSum}." +
+                                    $"\nДля выхода из программы введите {commandExit}.");
+
+                userInput = Console.ReadLine();
+
+                Console.Clear();
+
+                if (userInput == commandSum)
+                {
+                    GetSumNumbers(userNumbers);
+                }
+                else if (userInput == commandExit)
+                {
+                    isWork = false;
+                }
+                else
+                {
+                    AddNumber(userNumbers, userInput);
+                }
+
+                for (int i = 0; i < userNumbers.Count; i++)
+                {
+                    Console.Write(userNumbers[i] + " ");
+                }
+            }
+        }
+
+        static void AddNumber(List<int> userNumbers, string userInput)
+        {
+            if (Int32.TryParse(userInput, out int userNumber))
+            {
+                userNumbers.Add(userNumber);
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод.");
+            }
+        }
+
+        static void GetSumNumbers(List<int> userNumbers, int sum = 0)
+        {
+            foreach (int userNumber in userNumbers)
+            {
+                sum += userNumber;
+            }
+
+            Console.WriteLine($"Сумма ваших чисел: {sum}");
+        }
+    }
+}
